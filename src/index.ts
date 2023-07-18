73838707081be3d1eacae018f99c97c3e00dfed9
Patrick Fowler <pfowler@icloud.com>
2023-07-18 14:44:26 -0700

    feat: expose helper methods to set `enumMode` and `packageName`

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
 export { schema2typebox, Schema2TypeboxOptions } from "./programmatic-usage";
+
+import { setEnumMode, setPackageName } from "./schema-to-typebox";
+
+export const api = { setEnumMode, setPackageName }
