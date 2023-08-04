5421fa7cd32501208544c622cb0eb9a5569d4250
Patrick Fowler <fowler@shiftsmart.com>
2023-08-04 14:46:51 -0700

    test: add test coverage for nested object keys

--- test/enums.test.ts
+++ test/enums.test.ts
@@ -114,4 +114,61 @@ describe("object with enum", () => {
       `${expectedEnumCode}${expectedUnionCode}`
     );
   });
+
+  describe("... and nested object with same key enum", () => {
+    beforeEach(() => {
+      dummySchema = `{
+        "type": "object",
+        "properties": {
+          "status": {
+            "enum": [
+              "unknown",
+              "accepted",
+              "denied"
+            ]
+          },
+          "nested": {
+            "type": "object",
+            "properties": {
+              "status": {
+                "enum": [
+                  "hidden",
+                  "visible",
+                  "obscured"
+                ]
+              }
+            },
+            "required": [
+              "status"
+            ]
+          }
+        },
+        "required": [
+          "status"
+        ]
+      }`;
+    });
+
+    test("should generate unique union type names", () => {
+        setEnumMode("union");
+        const expectedTypebox = `
+            Type.Object({
+                status: StatusUnion, 
+                nested: Type.Object({
+                    status: NestedStatusUnion,
+                }),
+            })
+            `;
+
+        const schema = JSON.parse(dummySchema);
+        console.error('schema', schema)
+        const collected = collect(schema);
+        console.error('collected', collected)
+
+        expectEqualIgnoreFormatting(
+            collected,
+            expectedTypebox
+        );
+    });
+  });
 });
